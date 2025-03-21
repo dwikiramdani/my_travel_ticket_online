6bdae0f600e7c58baf786f3861daee92370a4f81
DwikiGramedia <148317395+DwikiGramedia@users.noreply.github.com>
2025-03-21 21:42:13 +0700

    Code Cleaning

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:my_travel_ticket_online/main.dart';
+import 'package:my_travel_ticket_online/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
